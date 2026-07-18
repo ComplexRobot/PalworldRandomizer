0afa60169ed4ea138c93a2a1f1c0bd7c7cc09f7e
ComplexRobot <complexrobotx@gmail.com>
2026-07-17 18:38:31 -0700

    Remove references to UAssetAPI from `PalSpawn.cs`.

--- PalSpawn.cs
+++ PalSpawn.cs
@@ -1,7 +1,5 @@
 using System.Text;
 using System.IO;
-using UAssetAPI;
-using UAssetAPI.ExportTypes;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
@@ -10,216 +8,6 @@ using System.Globalization;
 
 namespace PalworldRandomizer
 {
-    public static class PalSpawn
-    {
-        private static readonly byte[] dataSignature = [ 0, 0, 0, 0x80 ];
-        // Read in a SheetsVariant/PalSpawner uasset file to parse the binary and store spawn data.
-        public static SpawnExportData ReadAsset(UAsset uAsset, int? dataStart = null)
-        {
-            SpawnExportData spawnExportData = new();
-            RawExport rawExport = (RawExport) uAsset.Exports.Find(export => export is RawExport)!;
-            int startOffset;
-            if (dataStart == null)
-            {
-                startOffset = rawExport.Data.AsSpan(1).IndexOf(dataSignature);
-                if (startOffset == -1)
-                {
-                    throw new Exception($"{Path.GetFileNameWithoutExtension(uAsset.FilePath)}: Data signature not found.");
-                }
-            }
-            else
-            {
-                startOffset = dataStart.Value;
-            }
-            byte[] header = rawExport.Data[..startOffset];
-            int count = BitConverter.ToInt32(rawExport.Data, startOffset);
-            int position = startOffset + 4;
-            int end = position;
-            List<SpawnEntry> spawnEntries = [];
-            for (int i = 0; i < count;)
-            {
-                bool skipMinGroupSize = false;
-                bool skipMaxGroupSize = false;
-                if (rawExport.Data[position] == 0x80)
-                {
-                    ushort bitFlags = BitConverter.ToUInt16(rawExport.Data, position + 1);
-                    if (bitFlags == 0x0409 || bitFlags == 0x130D)
-                    {
-                        SpawnEntry spawnEntry = new();
-                        spawnEntries.Add(spawnEntry);
-                        spawnEntry.Weight = BitConverter.ToInt32(rawExport.Data, position + 3);
-                        spawnEntry.NightOnly = true;
-                        position += 14;
-                    }
-                    else if (bitFlags == 0x0509 || bitFlags == 0x170D)
-                    {
-                        SpawnEntry spawnEntry = new();
-                        spawnEntries.Add(spawnEntry);
-                        spawnEntry.Weight = 0;
-                        spawnEntry.NightOnly = true;
-                        position += 10;
-                    }
-                    else if (bitFlags == 0x0609 || bitFlags == 0x1B0D)
-                    {
-                        SpawnEntry spawnEntry = new();
-                        spawnEntries.Add(spawnEntry);
-                        spawnEntry.Weight = BitConverter.ToInt32(rawExport.Data, position + 3);
-                        position += 13;
-                    }
-                    else if (bitFlags == 0x0709 || bitFlags == 0x1F0D)
-                    {
-                        SpawnEntry spawnEntry = new();
-                        spawnEntries.Add(spawnEntry);
-                        spawnEntry.Weight = 0;
-                        position += 9;
-                    }
-                    else if (bitFlags == 0x100D)
-                    {
-                        skipMinGroupSize = true;
-                        position += 3;
-                    }
-                    else if (bitFlags == 0x200D)
-                    {
-                        skipMaxGroupSize = true;
-                        position += 3;
-                    }
-                    else
-                    {
-                        throw new Exception($"{Path.GetFileNameWithoutExtension(uAsset.FilePath)}: "
-                            + $"Unknown value 0x{Convert.ToHexString([.. rawExport.Data[(position + 1)..(position + 3)].Reverse()])} "
-                            + $"at offset 0x{Convert.ToHexString([.. BitConverter.GetBytes(position + 1).Reverse()])}.");
-                    }
-                }
-                else
-                {
-                    position += 2;
-                }
-                // TODO: Read in length of spawn entry to close the loop
-                if (position >= rawExport.Data.Length || (rawExport.Data[position - 1] != 0x0D && !(skipMinGroupSize || skipMaxGroupSize)))
-                {
-                    break;
-                }
-                SpawnData spawnData = new();
-                spawnEntries[^1].SpawnList.Add(spawnData);
-                int nameIndex = 0;
-                if (rawExport.Data[position] == 0x00)
-                {
-                    nameIndex = BitConverter.ToUInt16(rawExport.Data, position + 2);
-                }
-                else
-                {
-                    spawnData.IsPal = false;
-                    nameIndex = BitConverter.ToUInt16(rawExport.Data, position + 5);
-                }
-                spawnData.Name = uAsset.GetNameReference(nameIndex).Value;
-                position += 13;
-                spawnData.MinLevel = BitConverter.ToInt32(rawExport.Data, position);
-                position += 4;
-                spawnData.MaxLevel = BitConverter.ToInt32(rawExport.Data, position);
-                position += 4;
-                if (skipMinGroupSize)
-                {
-                    spawnData.MinCount = 0;
-                }
-                else
-                {
-                    spawnData.MinCount = BitConverter.ToInt32(rawExport.Data, position);
-                    position += 4;
-                }
-                if (skipMaxGroupSize)
-                {
-                    spawnData.MaxCount = 0;
-                }
-                else
-                {
-                    spawnData.MaxCount = BitConverter.ToInt32(rawExport.Data, position);
-                    position += 4;
-                }
-                if (position + 1 >= rawExport.Data.Length || (rawExport.Data[position] == 0x80 && rawExport.Data[position + 1] == 0x09))
-                {
-                    ++i;
-                }
-                end = position;
-            }
-            byte[] footer = rawExport.Data[end..];
-            spawnExportData.spawnEntries = spawnEntries;
-            spawnExportData.header = header;
-            spawnExportData.footer = footer;
-            return spawnExportData;
-        }
-
-        // Convert spawn data into binary data for saving as a uasset using the original uasset file header and footer.
-        public static void MutateAsset(UAsset uAsset, SpawnExportData spawnExportData)
-        {
-            List<byte> bytes = [.. spawnExportData.header, .. BitConverter.GetBytes(spawnExportData.spawnEntries.Count)];
-            foreach (SpawnEntry spawnEntry in spawnExportData.spawnEntries)
-            {
-                bytes.AddRange([0x80, 0x0D]);
-                if (spawnEntry.NightOnly && spawnEntry.Weight != 0)
-                {
-                    bytes.Add(0x13);
-                    bytes.AddRange(BitConverter.GetBytes(spawnEntry.Weight));
-                    bytes.Add(0x02);
-                }
-                else if (spawnEntry.NightOnly && spawnEntry.Weight == 0)
-                {
-                    bytes.Add(0x17);
-                    bytes.Add(0x02);
-                }
-                else if (spawnEntry.Weight != 0)
-                {
-                    bytes.Add(0x1B);
-                    bytes.AddRange(BitConverter.GetBytes(spawnEntry.Weight));
-                }
-                else if (spawnEntry.Weight == 0)
-                {
-                    bytes.Add(0x1F);
-                }
-                bytes.AddRange(BitConverter.GetBytes(spawnEntry.SpawnList.Count));
-                foreach (SpawnData spawnData in spawnEntry.SpawnList)
-                {
-                    int nameIndex = uAsset.AddNameReference(new(spawnData.Name, Encoding.ASCII));
-                    if (spawnData.MinCount == 0)
-                    {
-                        bytes.AddRange([0x80, 0x0D, 0x10]);
-                    }
-                    else if (spawnData.MaxCount == 0)
-                    {
-                        bytes.AddRange([0x80, 0x0D, 0x20]);
-                    }
-                    else
-                    {
-                        bytes.AddRange([0x00, 0x0D]);
-                    }
-                    if (spawnData.IsPal)
-                    {
-                        bytes.AddRange([0x00, 0x03]);
-                        bytes.AddRange(BitConverter.GetBytes((ushort) nameIndex));
-                        bytes.AddRange([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x03, 0x01]);
-                    }
-                    else
-                    {
-                        bytes.AddRange([0x80, 0x03, 0x01, 0x00, 0x03]);
-                        bytes.AddRange(BitConverter.GetBytes((ushort) nameIndex));
-                        bytes.AddRange([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
-                    }
-                    bytes.AddRange(BitConverter.GetBytes(spawnData.MinLevel));
-                    bytes.AddRange(BitConverter.GetBytes(spawnData.MaxLevel));
-                    if (spawnData.MinCount != 0)
-                    {
-                        bytes.AddRange(BitConverter.GetBytes(spawnData.MinCount));
-                    }
-                    if (spawnData.MaxCount != 0 || spawnData.MinCount == 0)
-                    {
-                        bytes.AddRange(BitConverter.GetBytes(spawnData.MaxCount));
-                    }
-                }
-            }
-            bytes.AddRange(spawnExportData.footer);
-            ((RawExport) uAsset.Exports.Find(export => export is RawExport)!).Data = [.. bytes];
-        }
-    }
-
     // A spawn group containing meta data and a list of character spawn data
     public class SpawnEntry
     {
@@ -385,9 +173,8 @@ namespace PalworldRandomizer
         public List<T> List => (List<T>) Items;
     }
 
-    public class AreaData(UAsset asset, SpawnExportData exportData, string name)
+    public class AreaData(SpawnExportData exportData, string name)
     {
-        public UAsset uAsset = asset;
         public SpawnExportData spawnExportData = exportData;
         public string filename = name;
         public int minLevel = 0;
@@ -413,7 +200,7 @@ namespace PalworldRandomizer
 
         public AreaData Clone()
         {
-            return new(new(), new(), filename)
+            return new(new(), filename)
             {
                 minLevel = minLevel,
                 maxLevel = maxLevel,
@@ -434,7 +221,6 @@ namespace PalworldRandomizer
                 isMonsterOnly = isMonsterOnly,
                 eggRespawnTime = eggRespawnTime,
                 eggLotteryCooldown = eggLotteryCooldown,
-                uAsset = isCage ? uAsset : UAssetData.LoadAsset($"Assets\\{filename}"),
                 spawnExportData =
                 new()
                 {
