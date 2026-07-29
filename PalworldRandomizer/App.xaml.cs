6abd6293317cbd7042c4aada7c55406bbc2cea85
ComplexRobot <complexrobotx@gmail.com>
2026-07-29 16:03:59 -0700

    Change to file-scoped namespace.

--- PalworldRandomizer/App.xaml.cs
+++ PalworldRandomizer/App.xaml.cs
@@ -5,126 +5,124 @@ using System.Windows.Input;
 using System.Windows.Threading;
 using PalworldRandomizer.Window;
 
-namespace PalworldRandomizer
+namespace PalworldRandomizer;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public static bool InstallationFolderVerified { get; private set; } = false;
+
+    private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs eventArgs)
     {
-        public static bool InstallationFolderVerified { get; private set; } = false;
-
-        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs eventArgs)
+        LogException(eventArgs.Exception);
+        try
         {
-            LogException(eventArgs.Exception);
-            try
-            {
-                Exception exception = eventArgs.Exception is TargetInvocationException invocationException ? invocationException.InnerException ?? eventArgs.Exception : eventArgs.Exception;
+            Exception exception = eventArgs.Exception is TargetInvocationException invocationException ? invocationException.InnerException ?? eventArgs.Exception : eventArgs.Exception;
 #if DEBUG
-                string message = exception.ToString();
-                Console.WriteLine(message[..message.TakeWhile(x => x != '\n').Count()]);
+            string message = exception.ToString();
+            Console.WriteLine(message[..message.TakeWhile(x => x != '\n').Count()]);
 #endif
-                MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            catch (Exception e)
-            {
-                LogException(e);
-            }
-            eventArgs.Handled = true;
+            MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
-
-        public static void LogException(Exception e)
+        catch (Exception e)
         {
-            try { File.AppendAllText(UAssetData.AppDataPath("error-log.txt"), $"{DateTime.Now}\n{e}\n\n\n"); } catch { }
+            LogException(e);
         }
+        eventArgs.Handled = true;
+    }
 
-        private void Application_Startup(object sender, StartupEventArgs e)
+    public static void LogException(Exception e)
+    {
+        try { File.AppendAllText(UAssetData.AppDataPath("error-log.txt"), $"{DateTime.Now}\n{e}\n\n\n"); } catch { }
+    }
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        Dispatcher.BeginInvoke(DispatcherPriority.Send, () =>
         {
-            Dispatcher.BeginInvoke(DispatcherPriority.Send, () =>
+            SettingsPage settingsPage = new();
+            AppWindow settingsWindow = new(() => settingsPage) { Title = "Settings", Topmost = true, Width = 800, Height = 200 };
+            settingsWindow.Closing += (sender, e) =>
             {
-                SettingsPage settingsPage = new();
-                AppWindow settingsWindow = new(() => settingsPage) { Title = "Settings", Topmost = true, Width = 800, Height = 200 };
-                settingsWindow.Closing += (sender, e) =>
+                if (InstallationFolderVerified)
                 {
-                    if (InstallationFolderVerified)
-                    {
-                        settingsWindow.HideClean();
-                        e.Cancel = true;
-                    }
-                    else
-                    {
-                        Shutdown();
-                    }
-                };
-                VerifyInstallationFolder(settingsWindow);
-            });
-        }
-
-        public void VerifyInstallationFolder(AppWindow settingsWindow)
-        {
-            if (!InstallationFolderVerified && UAssetData.VerifyInstallationFolder(settingsWindow))
-            {
-                InstallationFolderVerified = true;
-                ((AppWindow)SettingsPage.Instance.Parent)?.HideClean();
-                Initialize();
-            }
-        }
-
-        private void Initialize()
-        {
-            DispatcherOperation dataOperation = Dispatcher.BeginInvoke(DispatcherPriority.Background, async () =>
-            {
-                await using var fileProvider = await UAssetData.Initialize();
-                Data.Initialize(fileProvider);
-                Randomize.Initialize();
-
-                fileProvider.PostMount();
-
-                Randomize.RestoreBackup();
-                PalSpawnPage palSpawnpage = new();
-                AppWindow palSpawnWindow = new(() => palSpawnpage) { Title = "Pal Spawn Editor" };
-                palSpawnpage.ParentWindow = palSpawnWindow;
-                palSpawnWindow.Closing += (sender, e) =>
-                {
-                    palSpawnWindow.HideClean();
+                    settingsWindow.HideClean();
                     e.Cancel = true;
-                };
-            });
-            Dispatcher.BeginInvoke(DispatcherPriority.Send, () =>
-            {
-                MainWindow = new AppWindow(() => new MainPage(dataOperation)) { Title = "Palworld Randomizer", Width = 1280, Height = 800 };
-                MainWindow.Closed += (sender, e) => Shutdown();
-                ((AppWindow)MainWindow).ShowClean();
-            });
-            InputManager.Current.PreNotifyInput += SharedWindow.PreNotifyInput;
-            InputManager.Current.PostNotifyInput += SharedWindow.PostNotifyInput;
-        }
-
-        private void Application_Exit(object sender, ExitEventArgs e)
-        {
-            try
-            {
-                Randomize.SaveBackup();
-                if (MainPage.Instance == null)
-                {
-                    return;
-                }
-                ConfigData config = SharedWindow.GetConfig();
-                config.AutoRestoreTemplate = MainPage.Instance.autoSaveTemplate.IsChecked == true;
-                config.OutputLog = MainPage.Instance.outputLog.IsChecked == true;
-                SharedWindow.SaveConfig(config);
-                if (config.AutoRestoreTemplate)
-                {
-                    MainPage.Instance.ValidateFormData();
-                    MainPage.SaveTemplate(new FormData(MainPage.Instance), UAssetData.AppDataPath(MainPage.AUTO_TEMPLATE_FILENAME));
                 }
                 else
                 {
-                    File.Delete(UAssetData.AppDataPath(MainPage.AUTO_TEMPLATE_FILENAME));
+                    Shutdown();
                 }
-            }
-            catch (Exception exception)
-            {
-                LogException(exception);
-            }
+            };
+            VerifyInstallationFolder(settingsWindow);
+        });
+    }
+
+    public void VerifyInstallationFolder(AppWindow settingsWindow)
+    {
+        if (!InstallationFolderVerified && UAssetData.VerifyInstallationFolder(settingsWindow))
+        {
+            InstallationFolderVerified = true;
+            ((AppWindow)SettingsPage.Instance.Parent)?.HideClean();
+            Initialize();
         }
     }
 
+    private void Initialize()
+    {
+        DispatcherOperation dataOperation = Dispatcher.BeginInvoke(DispatcherPriority.Background, async () =>
+        {
+            await using var fileProvider = await UAssetData.Initialize();
+            Data.Initialize(fileProvider);
+            Randomize.Initialize();
+
+            fileProvider.PostMount();
+
+            Randomize.RestoreBackup();
+            PalSpawnPage palSpawnpage = new();
+            AppWindow palSpawnWindow = new(() => palSpawnpage) { Title = "Pal Spawn Editor" };
+            palSpawnpage.ParentWindow = palSpawnWindow;
+            palSpawnWindow.Closing += (sender, e) =>
+            {
+                palSpawnWindow.HideClean();
+                e.Cancel = true;
+            };
+        });
+        Dispatcher.BeginInvoke(DispatcherPriority.Send, () =>
+        {
+            MainWindow = new AppWindow(() => new MainPage(dataOperation)) { Title = "Palworld Randomizer", Width = 1280, Height = 800 };
+            MainWindow.Closed += (sender, e) => Shutdown();
+            ((AppWindow)MainWindow).ShowClean();
+        });
+        InputManager.Current.PreNotifyInput += SharedWindow.PreNotifyInput;
+        InputManager.Current.PostNotifyInput += SharedWindow.PostNotifyInput;
+    }
+
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        try
+        {
+            Randomize.SaveBackup();
+            if (MainPage.Instance == null)
+            {
+                return;
+            }
+            ConfigData config = SharedWindow.GetConfig();
+            config.AutoRestoreTemplate = MainPage.Instance.autoSaveTemplate.IsChecked == true;
+            config.OutputLog = MainPage.Instance.outputLog.IsChecked == true;
+            SharedWindow.SaveConfig(config);
+            if (config.AutoRestoreTemplate)
+            {
+                MainPage.Instance.ValidateFormData();
+                MainPage.SaveTemplate(new FormData(MainPage.Instance), UAssetData.AppDataPath(MainPage.AUTO_TEMPLATE_FILENAME));
+            }
+            else
+            {
+                File.Delete(UAssetData.AppDataPath(MainPage.AUTO_TEMPLATE_FILENAME));
+            }
+        }
+        catch (Exception exception)
+        {
+            LogException(exception);
+        }
+    }
 }
